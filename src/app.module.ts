86e302d9d8fb0086deca97e6c5ce1f5258133b4c
dprastha <danielprastha4@gmail.com>
2022-03-30 11:21:00 +0700

    set up connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env`],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        autoLoadEntities: true,
+        synchronize: true,
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+      }),
+    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   autoLoadEntities: true,
+    //   synchronize: true,
+    // }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

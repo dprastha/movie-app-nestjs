f75101960ec98dace794b29c8995bc4f4444e357
dprastha <danielprastha4@gmail.com>
2022-03-30 17:22:55 +0700

    finish crud tags

---
+++ src/tags/entities/tag.entity.ts
@@ -0,0 +1,33 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Tag {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+  })
+  updatedAt: Date;
+
+  @DeleteDateColumn({
+    type: 'timestamp',
+    nullable: true,
+  })
+  onDelete: Date;
+}
---
+++ src/tags/tags.service.ts
@@ -0,0 +1,54 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateTagDto } from './dto/create-tag.dto';
+import { UpdateTagDto } from './dto/update-tag.dto';
+import { Tag } from './entities/tag.entity';
+import { TagsRepository } from './tags.repository';
+
+@Injectable()
+export class TagsService {
+  constructor(
+    @InjectRepository(TagsRepository)
+    private readonly tagRepository: TagsRepository,
+  ) {}
+  findAll(): Promise<Tag[]> {
+    return this.tagRepository.getTags();
+  }
+
+  create(createTagDto: CreateTagDto): Promise<Tag> {
+    const { name } = createTagDto;
+    const tag = this.tagRepository.create({
+      name,
+    });
+
+    return this.tagRepository.save(tag);
+  }
+
+  async findOne(id: number): Promise<Tag> {
+    const found = this.tagRepository.findOne({ where: { id } });
+
+    if (!found) {
+      throw new NotFoundException(`Tag with ID "${id}" not found`);
+    }
+
+    return found;
+  }
+
+  async update(id: number, updateTagDto: UpdateTagDto): Promise<Tag> {
+    const { name } = updateTagDto;
+    const tag = await this.findOne(id);
+
+    tag.name = name;
+
+    await this.tagRepository.save(tag);
+    return tag;
+  }
+
+  async remove(id: number): Promise<void> {
+    const result = await this.tagRepository.softDelete(id);
+
+    if (result.affected === 0) {
+      throw new NotFoundException();
+    }
+  }
+}
